e5e64b0f4e2c31c59170889e548b6a86de86a5bc
gulmina <gulmina44@gmail.com>
2024-07-03 10:28:24 +0200

    frontend added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import "./index.css";
+import Home from "./pages/Home";
+import Allpokemon from "./pages/Allpokemon";
+import Singlepokemon from "./pages/Singlepokemon";
+import Pokemoninfo from "./pages/Pokemoninfo";
+
+/* import Player from "./pages/Player.jsx"; */
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon" element={<Allpokemon />} />
+        <Route path="/pokemon/:id" element={<Singlepokemon />} />
+        <Route path="/pokemon/:id/:info" element={<Pokemoninfo />} />
+        {/*  <Route path="/pokemon/playerselect" element={<Player />} /> */}
+        <Route path="/*" element={<div>Error</div>} />
+      </Routes>
+    </>
+  );
+}
+export default App;
---
+++ frontend/src/pages/Singlepokemon.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+//import backgroundimg from "../components/Background.jsx";
+
+const Singlepokemon = () => {
+  const [pokemonId, setpokemonId] = useState([]);
+  const { id } = useParams();
+  console.log(id);
+
+  const getpokemondata = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/pokemon/${id}`);
+      console.log(response.data);
+
+      setpokemonId(response.data);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  //const pokemonbase = pokemonId.base;
+
+  useEffect(() => {
+    getpokemondata();
+  }, []);
+  console.log(pokemonId);
+  return (
+    <div>
+      <div className="flex justify-center items-center container-single">
+        <div className="bg-sky-500/100 p-4 rounded-lg">
+          <h1 className="text-2xl font-bold">Single Pokemon Data</h1>
+          <p>
+            <strong>ID:</strong> {pokemonId.id}
+          </p>
+
+          <p>
+            <strong>Name :</strong>
+            {pokemonId.name &&
+              Object.values(pokemonId.name)?.map((item, index) => (
+                <li key={index}> {item} </li>
+              ))}
+          </p>
+          <p>
+            <strong>BASE :</strong>
+            {pokemonId.base &&
+              Object.entries(pokemonId.base)?.map((item, index) => (
+                <li key={index}>
+                  {item[0]} : {item[1]}
+                </li>
+              ))}
+          </p>
+          <p>
+            <strong>Type:</strong> {pokemonId?.type}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Singlepokemon;
