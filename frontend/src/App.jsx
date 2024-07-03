946ed88fffb1135aada0d4e727fe8e75fa52a595
nospike4u <nospike4u@hotmail.com>
2024-07-03 13:44:32 +0200

    pokemon game page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Allpokemon from "./pages/Allpokemon";
 import Singlepokemon from "./pages/Singlepokemon.jsx";
 import Pokemoninfo from "./pages/Pokemoninfo";
+import Game from "./pages/Game.jsx";
 
 /* import Player from "./pages/Player.jsx"; */
 
@@ -16,7 +17,7 @@ function App() {
         <Route path="/pokemon" element={<Allpokemon />} />
         <Route path="/pokemon/:id" element={<Singlepokemon />} />
         <Route path="/pokemon/:id/:info" element={<Pokemoninfo />} />
-        {/*  <Route path="/pokemon/playerselect" element={<Player />} /> */}
+         <Route path="/pokemon/game" element={<Game />} />
         <Route path="/*" element={<div>Error</div>} />
       </Routes>
     </>
