b9c05fa4d76b4759ef0ae8f121d0fb4ff9893f74
gulmina <gulmina44@gmail.com>
2024-07-07 23:54:44 +0200

    Savegame information into the backend component added

---
+++ frontend/src/components/Savegame.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Savegame = ({ player }) => {
+  console.log(player);
+  const newdata = {
+    name: `${player}`,
+    winner: 0,
+    loser: 1,
+    games: 1,
+  };
+
+  const handleClick = (message) => {
+    const response = fetch("http://localhost:8000/game/savegame", {
+      method: "POST",
+      body: JSON.stringify(message),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
+    if (response.status !== 201) {
+      throw new Error("Faield to post");
+    }
+    const post = response.json();
+    console.log(post);
+    return post;
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>
+          <button
+            className="border-solid border-2 border-indigo-200 rounded-md p-2 text-lg"
+            onClick={() => handleClick(newdata)}
+          >
+            End Game
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Savegame;
