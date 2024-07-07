12dffbd3f6a8f577c18e126a046032e69626b344
gulmina <gulmina44@gmail.com>
2024-07-07 22:18:46 +0200

    header component modified

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import axios from "axios";
 
 const Leaderboard = () => {
-    return (
-      <>
-        <h1>
-            This is Leaderboard page!
-        </h1>
-      </>
-    );
-  };
-  
-  export default Leaderboard;
+  /*  const data = new FormData();
+  formData.append("first_name", profile.firstName);
+  formData.append("last_name", profile.lastName);
+  formData.append("email", profile.email);
+
+  return fetch("http://example.com/api/v1/registration", {
+    method: "POST",
+    body: formData,
+  }).then((response) => response.json());
+
+  createNewProfile(profile)
+    .then((json) => {
+      // handle success
+    })
+    .catch((error) => error); */
+  return (
+    <>
+      <h1>This is Leaderboard page!</h1>
+    </>
+  );
+};
+
+export default Leaderboard;
